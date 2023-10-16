e63be552a426611ce86b67af49e5c9236a5a07d2
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-10-16 16:59:45 +0300

    Task2

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -6,8 +6,9 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
     int addDays = 4;
@@ -16,15 +17,18 @@ public class CardDeliveryTest {
     @Test
     public void shouldBeSuccessfully(){
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Сыктывкар");
+        String day = Integer.toString(LocalDate.now().plusDays(addDays).getDayOfMonth());
+        $("[data-test-id='city'] input").setValue("Сы");
+        $$("span.menu-item__control").find(exactText("Сыктывкар")).click();
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(planningDate);
+        $("span.icon").click();
+        $$("tr.calendar__row td.calendar__day").find(exactText(day)).click();
         $("[data-test-id='name'] input").setValue("Ворошилова Екатерина Анатольевна");
         $("[data-test-id='phone'] input").setValue("+79048631049");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
